d35225620e91f16e8304d77192e8fdec7b3bab91
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-03 12:04:17 -0400

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
-var smallGrou = ["adam","josh","courtney","jocelle"];
+var smallGroup = ["adam","josh","courtney","jocelle"];
 var birthYears = [95,93,60,65];
-var yesNO = [true,false,true,false];
+var yesNo = [true,false,true,false];
+
+//remove the last item "jocelle" from the array
+smallGroup.pop();
+
+//add a new item to the end of the array
+birthYears.push(85);
+
+//remove the first item from the array
+yesNo.shift();
+
+console.log(smallGroup);
+console.log(birthYears);
+console.log(yesNo);
